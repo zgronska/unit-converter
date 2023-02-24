c2fa27cec05468096e3cf468d386ef5c8e7dea32
zgronska <zgronska@gmail.com>
2023-02-24 23:31:40 +0100

    Finished rendering function, working on converting

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+// Converter
+
+// Select elements
+const petAgeInput = document.querySelector("#age");
+const dogOpt = document.querySelector("#dog");
+const catOpt = document.querySelector("#cat");
+const convertBtn = document.querySelector(".btn-convert");
+const resultEl = document.querySelector(".result-container");
+const resultElTitle = document.querySelector(".result-title");
+const resultElContent = document.querySelector(".result-content");
+
+// Declare variables of user's pet
+let petAge;
+let petType;
+
+// Add an event listener to trigger the conversion
+convertBtn.addEventListener("click", function () {
+  // Assign the inputted values
+  petAge = petAgeInput.value;
+  petType = dogOpt.checked ? "dog" : "cat";
+  // call function
+  convertAge(petAge, petType);
+});
+
+// Convert pet years
+function convertAge(age, pet) {
+  // Declare a variable for the result
+  let humanAge;
+
+  // Check if age isn't below 1
+  if (age >= 1) {
+    // Check if it's a dog or a cat
+    if (pet === "dog") {
+      // Source: https://www.akc.org/expert-advice/health/how-to-calculate-dog-years-to-human-years/
+      humanAge = Math.round(16 * Math.log(age) + 31);
+    } else if (pet === "cat") {
+      //code
+    }
+  }
+  console.log(humanAge);
+  // Call rendering function
+  renderResult(humanAge);
+}
+
+function renderResult(age) {
+  resultElTitle.textContent = `${petType === "dog" ? "Dog" : "Cat"} age`;
+  resultElContent.textContent = `Your ${petAge} year old ${petType} is ${age} in human years!`;
+}
+
 // Dark mode toggle
 const btnTheme = document.querySelector(".btn-theme");
 
@@ -6,7 +55,7 @@ const savedMode = localStorage.getItem("mode");
 const systemMode = window.matchMedia("(prefers-color-scheme: dark)").matches // Check user's system preferences for a mode
   ? "dark"
   : "light";
-const currentMode = savedMode || systemMode; // Use the saved mode or system mode, defaulting to light mode
+const currentMode = savedMode || systemMode; // Use the saved mode or system mode
 
 // Set the theme mode based on the current mode
 document.documentElement.classList.toggle("dark", currentMode === "dark"); // Set the dark class on the root element if current mode is dark
@@ -16,7 +65,7 @@ btnTheme.classList.toggle("fa-moon", currentMode === "dark");
 
 // Add an event listener to toggle the theme on click
 btnTheme.addEventListener("click", function () {
-  btnTheme.classList.toggle("triggered"); // Toggle the rotation animation on the toggle button
+  btnTheme.classList.toggle("triggered"); // Trigger the rotation animation
   const isDarkMode = document.documentElement.classList.toggle("dark"); // Toggle the dark class on the root element and check if the mode is now dark
   localStorage.setItem("mode", isDarkMode ? "dark" : "light"); // Save the current mode in local storage
   // Toggle the icons
